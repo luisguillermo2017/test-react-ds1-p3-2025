cdbd2bfad415f93d969017ad66ee2e30b11e5b21
Luis Guillermo Loaiza Cordero <luisguillermo2017@gmail.com>
2025-06-04 20:43:20 -0600

    Creación del componente Titulo y su llamado en App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Titulo from './components/Titulo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Titulo titulo="Hola mundo desde el componente titulo" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
