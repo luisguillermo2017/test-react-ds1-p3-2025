6a2a1b7d5644532f44d11e2c34e74b195f69962c
Rafael Garcia <rafalbergh@gmail.com>
2025-06-04 20:43:41 -0600

    Creación de componente tarjeta y llamado en app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Tarjeta from './components/Tarjeta';
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,8 @@ function App() {
         >
           Learn React
         </a>
+        <Tarjeta nombre="Rafael" descripcion="Esta es un descripción de prueba"/>
+
       </header>
     </div>
   );
