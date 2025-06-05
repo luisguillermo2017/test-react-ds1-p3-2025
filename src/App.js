2ac3c588634cd78e29c6c689169ce2e1ac48b0bc
Angel Matamoros <angel.matamorosjimenez@ucreativa.com>
2025-06-04 20:46:49 -0600

    se agrega el cardwelcome.jsx y uso en app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import CardWelcome from './components/cardwelcome';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <CardWelcome />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/cardwelcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardWelcome() {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+    </div>
+  );
+}
+
+export default CardWelcome;
